ec1f43da004164c6f4a8739e467a9245e94e4534
Luke <luke.hanuska@gmail.com>
2024-09-14 00:46:00 -0400

    Adjusted header button style

--- market-sprout/components/PageHeader.tsx
+++ market-sprout/components/PageHeader.tsx
@@ -1,4 +1,17 @@
+import Link from "next/link";
 import styles from "../styles/Page.module.css";
 export default function PageHeader() {
-  return <div className={styles.header}>HEADER</div>;
+  return (
+    <div className={styles.header}>
+      <Link href={"/"} className={styles.button}>
+        Home
+      </Link>
+      <Link href={"/services"} className={styles.button}>
+        Services
+      </Link>
+      <Link href={"/about"} className={styles.button}>
+        Home
+      </Link>
+    </div>
+  );
 }
