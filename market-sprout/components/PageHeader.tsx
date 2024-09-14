cc4c4702613f7b4843afc9abcc6a84cfb02aac46
Luke <luke.hanuska@gmail.com>
2024-09-14 00:30:12 -0400

    Configured app navigation and set up page designs

---
+++ market-sprout/components/PageHeader.tsx
@@ -0,0 +1,4 @@
+import styles from "../styles/Page.module.css";
+export default function PageHeader() {
+  return <div className={styles.header}>HEADER</div>;
+}
---
+++ market-sprout/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PageComponent from "../components/PageComponent";
+
+export default function Home() {
+  return (
+    <PageComponent>
+      <div>HOME PAGE</div>
+    </PageComponent>
+  );
+}
