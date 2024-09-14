584cb3e8c70d64b7f833a6b99e18fe3ce640ad90
Luke <luke.hanuska@gmail.com>
2024-09-14 14:52:45 -0400

    Added image as background and adjusted main page

--- market-sprout/pages/index.tsx
+++ market-sprout/pages/index.tsx
@@ -3,6 +3,7 @@ import PageComponent from "../components/PageComponent";
 import styles from "../styles/Page.module.css";
 import { EventEmitter } from "stream";
 import { IoSparklesOutline } from "react-icons/io5";
+import { symlink } from "fs";
 
 const DEFAULT_BUDGET = 100;
 const DEFAULT_DURATION = 1;
@@ -16,11 +17,15 @@ export default function Home() {
     }
   };
   const handleOnBlur = (event: React.FocusEvent<HTMLInputElement, Element>) => {
-    const value = Number(
+    let value = Number(
       event.currentTarget.value.length > 0
         ? event.currentTarget.value
         : DEFAULT_BUDGET
     );
+    if (value < 1) {
+      value = DEFAULT_BUDGET;
+    }
+
     if (value == undefined || isNaN(value)) {
       alert("Budget value is undefined or is not a number.");
       event.currentTarget.value = DEFAULT_BUDGET.toFixed(2);
@@ -37,33 +42,37 @@ export default function Home() {
   };
   return (
     <PageComponent>
-      <div className="background">
-        <div className={styles.contentContainer}>
-          <h1>Market-Sprout</h1>
-          <p>Sustainable, Fresh, Local</p>
-          <div className={styles.budgetContainer}>
-            <p>Budget $</p>
-            <input
-              type="number"
-              placeholder={"100.00"}
-              onKeyUp={handleKeyUp}
-              onBlur={handleOnBlur}
-            />
-          </div>
-          <div className={styles.durationContainer}>
-            <p>Duration: {duration + (duration > 1 ? " Days" : " Day")}</p>
-            <input
-              type="range"
-              min={1}
-              max={14}
-              step={1}
-              onChange={handleSliderChanged}
-              defaultValue={DEFAULT_DURATION}
-            />
-          </div>
-          <div className={styles.generateButton}>
-            <IoSparklesOutline size={"2vw"} />
-            Generate
+      <div className={styles.backdrop}>
+        <div className={styles.frosted}>
+          <div className={styles.contentContainer}>
+            <h1>Market-Sprout</h1>
+            <p>Sustainable, Fresh, Local</p>
+            <div style={{ height: 20 }} />
+            <div className={styles.budgetContainer}>
+              <p>Budget $</p>
+              <input
+                type="number"
+                placeholder={"100.00"}
+                onKeyUp={handleKeyUp}
+                onBlur={handleOnBlur}
+              />
+            </div>
+
+            <div className={styles.durationContainer}>
+              <p>Duration: {duration + (duration > 1 ? " Days" : " Day")}</p>
+              <input
+                type="range"
+                min={1}
+                max={14}
+                step={1}
+                onChange={handleSliderChanged}
+                defaultValue={DEFAULT_DURATION}
+              />
+            </div>
+            <div className={styles.generateButton}>
+              <IoSparklesOutline size={"2vw"} />
+              Generate
+            </div>
           </div>
         </div>
       </div>
